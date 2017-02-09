6cd49e36a09162fa7eeb2fad80f0d32847398996
John Hawley <jhawley@institute.local>
2017-02-08 19:09:21 -0600

    created ball object and script

---
+++ Assets/Scripts/BallController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallController : MonoBehaviour {
+
+	public float speed = 2.0f;
+
+	// Use this for initialization
+	void Start () {
+		Rigidbody body = GetComponent<Rigidbody> ();
+		body.velocity = Vector3.up;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+			
+	}
+}
