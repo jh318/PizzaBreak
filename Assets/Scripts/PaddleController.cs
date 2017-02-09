0d736b2d04895f318d5e97be025f6ef6ae5e4c33
John Hawley <jhawley@institute.local>
2017-02-08 19:48:16 -0600

    added tilt

--- Assets/Scripts/PaddleController.cs
+++ Assets/Scripts/PaddleController.cs
@@ -5,9 +5,11 @@ using UnityEngine;
 public class PaddleController : MonoBehaviour {
 
 	public float speed = 2.0f;
+	public float tilt = 3;
 
 	void Update () {
 		float playerInputX = Input.GetAxis ("Horizontal") * speed * Time.deltaTime; 
 		transform.position = transform.position + new Vector3(playerInputX, 0, 0); // current position + Vector3.right * playerInputX
+		transform.localEulerAngles = Vector3.back * tilt * playerInputX;
 	}
 }
