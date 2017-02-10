dca923a96c6865c42b76ac68195001482cc5864c
John Hawley <jhawley@institute.local>
2017-02-09 18:47:28 -0600

    Created UI basics. Re-added GameManager. Extemded brickcontroller.

--- Assets/Scripts/BrickController.cs
+++ Assets/Scripts/BrickController.cs
@@ -3,9 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class BrickController : MonoBehaviour {
-
+	public int points = 10;
 	
 	void OnCollisionExit(Collision collision){
 		gameObject.SetActive (false);
+		GameManager.BrickBroken (points);
+
 	}
 }
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,10 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour {
 
 	public static GameManager instance;
+
+	public Text livesText;
+	public Text scoreText;
+	public Text highScoreText;
 
 	public int lives = 3;
 	public int score = 0;
@@ -20,9 +25,19 @@ public class GameManager : MonoBehaviour {
 		Debug.Log ("My score: " + score);
 		Debug.Log ("Manager score: " + instance.score);
 	}
+
+	void Start (){
+		livesText.text = "Lives: " + lives;
+		scoreText.text = "Scores: " + score;
+	}
 	public static void LostBall(){
 		instance.lives--;
-		Debug.Log ("ball off screen");
+		instance.livesText.text = "Lives: " + instance.lives;
+	}
+
+	public static void BrickBroken (int points){
+		instance.score += points;
+		instance.scoreText.text = "Score: " + instance.score;
 	}
 
 }
